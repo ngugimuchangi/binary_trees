628db8364cb6b4d4abf23dc201fcc84841e9e0bd
ngugimuchangi <tech.curious14@gmail.com>
2023-08-15 01:31:23 +0300

    code cleanup

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
+
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
