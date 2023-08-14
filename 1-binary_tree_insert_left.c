7a99802a2305ae3749c58a0835f9f772851bf534
ngugimuchangi <tech.curious14@gmail.com>
2023-08-15 01:29:56 +0300

    free allocated node memory if no parent and return null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,17 +11,18 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp = NULL;
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp = NULL, *node = NULL;
 
+	if (!parent)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
-	if (!parent)
-		return (node);
 	if (parent->left)
 	{
 		temp = parent->left;
