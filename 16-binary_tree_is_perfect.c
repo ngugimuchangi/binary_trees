2b201608c06a4733d95054f9620c777c0ed1e4d4
ngugimuchangi <tech.curious14@gmail.com>
2023-08-16 21:13:32 +0300

    fixed perfect tree conditions verification

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
-size_t depth(const binary_tree_t *tree);
-int is_perfect(const binary_tree_t *tree, size_t depth, size_t max_depth);
+size_t get_depth(const binary_tree_t *tree);
+int is_perfect(const binary_tree_t *tree, size_t level, size_t depth);
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -12,41 +11,41 @@ int is_perfect(const binary_tree_t *tree, size_t depth, size_t max_depth);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t max_depth = depth(tree);
+	size_t depth = get_depth(tree);
 
 	if (!tree)
 		return (0);
-	return (is_perfect(tree, 0, max_depth));
+	return (is_perfect(tree, 0, depth));
 }
 
 /**
  * is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
- * @depth: depth of the current node
- * @max_depth: depth of the deepest node
+ * @level: depth of the current node
+ * @depth: depth of the deepest node
  *
  * Return: 1 if tree is perfect, 0 otherwise
  */
-int is_perfect(const binary_tree_t *tree, size_t depth, size_t max_depth)
+int is_perfect(const binary_tree_t *tree, size_t level, size_t depth)
 {
 	if (!tree)
 		return (1);
-	if ((!tree->left && tree->right) || (tree->left && !tree->right))
+	if (!tree->left && !tree->right)
+		return (level == depth);
+	if (!tree->left || !tree->right)
 		return (0);
-	if (depth > max_depth)
-		return (0);
-	depth += 1;
-	return (is_perfect(tree->left, depth, max_depth) &&
-			is_perfect(tree->right, depth, max_depth));
+	level += 1;
+	return (is_perfect(tree->left, level, depth) &&
+			is_perfect(tree->right, level, depth));
 }
 
 /**
- * depth - measures the depth of a node in a binary tree
+ * get_depth - measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
  *
  * Return: depth of the node, 0 if tree is NULL
  */
-size_t depth(const binary_tree_t *tree)
+size_t get_depth(const binary_tree_t *tree)
 {
 	binary_tree_t *node = (binary_tree_t *)tree;
 	size_t depth = 0;
