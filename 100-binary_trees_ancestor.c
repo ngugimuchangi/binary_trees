734e11c890b7ffc615f0153c240b0775b5184623
ngugimuchangi <tech.curious14@gmail.com>
2023-08-17 02:23:36 +0300

    add detailed description

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,11 @@
 #include "binary_trees.h"
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * Description: The lowest common ancestor is the farthest node from the root
+ * that is an ancestor of both nodes. This solution give O(n^2) time complexity
+ * but it can be improve with a hash set to store the ancestors of the first
+ * then traverse the ancestors of the second to find the first common ancestor
+ * resulting in O(n) time complexity.
  * @first: pointer to the first node
  * @second: pointer to the second node
  * Return: pointer to the lowest common ancestor node of the two given nodes
