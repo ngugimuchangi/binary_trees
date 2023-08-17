c8973f1b3133c4954709d5b1806367edb95eb8f3
ngugimuchangi <tech.curious14@gmail.com>
2023-08-18 02:07:12 +0300

    establish whether a tree is binary search tree

--- binary_trees.h
+++ binary_trees.h
@@ -69,5 +69,10 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+
+/* Helper functions - Moved here because of Betty */
+void create_queue(binary_tree_t *tree, q_node **head, q_node **tail,
+				  q_node *(*func)(binary_tree_t *, q_node **, q_node **));
 
 #endif /* BINARY_TREES_H */
