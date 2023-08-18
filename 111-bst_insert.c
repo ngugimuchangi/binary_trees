0a14c8a4b3ffa613e203604aff6ffb7549c6beb7
ngugimuchangi <tech.curious14@gmail.com>
2023-08-18 03:33:28 +0300

    code cleanup

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+
 /**
  * bst_insert - inserts a value in a Binary Search Tree
  * @tree: double pointer to the root node of the BST to insert the value
