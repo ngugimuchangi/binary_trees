e499598743d57590717418dfab6363b75b80a4d6
ngugimuchangi <tech.curious14@gmail.com>
2023-08-24 21:05:12 +0300

    modify helper function names -> resolve conflict

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-int get_total_nodes(const binary_tree_t *tree);
-void heapify(heap_t **arr, int index, int size);
-void array_from_heap(heap_t **arr, heap_t *root, int index, int size);
-heap_t *create_heap(heap_t **arr, heap_t *parent, int index, int size);
+int node_count(const binary_tree_t *tree);
+void max_heapify(heap_t **arr, int index, int size);
+void max_heap_to_array(heap_t **arr, heap_t *root, int index, int size);
+heap_t *array_to_max_heap(heap_t **arr, heap_t *parent, int index, int size);
 
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
@@ -14,7 +14,7 @@ heap_t *create_heap(heap_t **arr, heap_t *parent, int index, int size);
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	int i, node_count;
+	int i, size;
 	heap_t **nodes_array = NULL, *new_node = NULL;
 
 	if (!root)
@@ -24,36 +24,36 @@ heap_t *heap_insert(heap_t **root, int value)
 	if (!new_node)
 		return (NULL);
 
-	node_count = get_total_nodes(*root) + 1;
-	nodes_array = malloc(sizeof(heap_t **) * node_count);
+	size = node_count(*root) + 1;
+	nodes_array = malloc(sizeof(heap_t **) * size);
 	if (!nodes_array)
 		return (NULL);
 
-	array_from_heap(nodes_array, *root, 0, node_count - 1);
-	nodes_array[node_count - 1] = new_node;
+	max_heap_to_array(nodes_array, *root, 0, size - 1);
+	nodes_array[size - 1] = new_node;
 
-	for (i = (node_count / 2) - 1; i >= 0; i--)
-		heapify(nodes_array, i, node_count);
+	for (i = (size / 2) - 1; i >= 0; i--)
+		max_heapify(nodes_array, i, size);
 
-	*root = create_heap(nodes_array, NULL, 0, node_count);
+	*root = array_to_max_heap(nodes_array, NULL, 0, size);
 	free(nodes_array);
 	return (new_node);
 }
 
 /**
- * get_total_nodes - Counts the total number of nodes in a binary tree
+ * node_count - Counts the total number of nodes in a binary tree
  * @tree: Pointer to the root node of the tree to count the number of nodes
  * Return: Number of nodes in the tree
  */
-int get_total_nodes(const heap_t *tree)
+int node_count(const heap_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (1 + get_total_nodes(tree->left) + get_total_nodes(tree->right));
+	return (1 + node_count(tree->left) + node_count(tree->right));
 }
 
 /**
- * array_from_heap - Creates an array of nodes from a Max Binary Heap
+ * max_heap_to_array - Creates an array of nodes from a Max Binary Heap
  *
  * @root: Pointer to the root node of the Heap to convert
  * @arr: Pointer to the array to be created from the Heap
@@ -62,17 +62,17 @@ int get_total_nodes(const heap_t *tree)
  *
  * Return: Nothing
  */
-void array_from_heap(heap_t **arr, heap_t *root, int index, int size)
+void max_heap_to_array(heap_t **arr, heap_t *root, int index, int size)
 {
 	if (index >= size)
 		return;
 	arr[index] = root;
-	array_from_heap(arr, root->left, 2 * index + 1, size);
-	array_from_heap(arr, root->right, 2 * index + 2, size);
+	max_heap_to_array(arr, root->left, 2 * index + 1, size);
+	max_heap_to_array(arr, root->right, 2 * index + 2, size);
 }
 
 /**
- * create_heap - Creates a Max Binary Heap tree from an array
+ * array_to_max_heap - Creates a Max Binary Heap tree from an array
  *
  * @arr: Pointer to the array to be converted
  * @parent: Pointer to the parent node
@@ -81,7 +81,7 @@ void array_from_heap(heap_t **arr, heap_t *root, int index, int size)
  *
  * Return: Pointer to the root node of the created Max Binary Heap
  */
-heap_t *create_heap(heap_t **arr, heap_t *parent, int index, int size)
+heap_t *array_to_max_heap(heap_t **arr, heap_t *parent, int index, int size)
 {
 	heap_t *node;
 
@@ -89,14 +89,14 @@ heap_t *create_heap(heap_t **arr, heap_t *parent, int index, int size)
 		return (NULL);
 	node = arr[index];
 	node->parent = parent;
-	node->left = create_heap(arr, node, 2 * index + 1, size);
-	node->right = create_heap(arr, node, 2 * index + 2, size);
+	node->left = array_to_max_heap(arr, node, 2 * index + 1, size);
+	node->right = array_to_max_heap(arr, node, 2 * index + 2, size);
 
 	return (node);
 }
 
 /**
- * heapify - Converts an array into a Max Binary Heap
+ * max_heapify - Converts an array into a Max Binary Heap
  *
  * @arr: Pointer to the first element of the array to be converted
  * @index: Index of current node
@@ -104,7 +104,7 @@ heap_t *create_heap(heap_t **arr, heap_t *parent, int index, int size)
  *
  * Return: Nothing
  */
-void heapify(heap_t **arr, int index, int size)
+void max_heapify(heap_t **arr, int index, int size)
 {
 	int largest = index, l = index * 2 + 1, r = index * 2 + 2;
 	heap_t *temp = NULL;
@@ -118,6 +118,6 @@ void heapify(heap_t **arr, int index, int size)
 		temp = arr[index];
 		arr[index] = arr[largest];
 		arr[largest] = temp;
-		heapify(arr, largest, size);
+		max_heapify(arr, largest, size);
 	}
 }
